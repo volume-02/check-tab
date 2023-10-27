77796b775978706435fc6b76fdfc92a8426d56fe
volume02 <borsukboris@gmail.com>
2023-10-27 16:10:42 +0700

    onFocus & onUpdate handling

--- check-tab.js
+++ check-tab.js
@@ -1,18 +1,45 @@
-import { post } from "./utils.js";
+import { post } from './utils.js';
 
 let queryOptions = { active: true, lastFocusedWindow: true };
 
+console.log(chrome.tabs);
 
-chrome.tabs.onActivated.addListener(() => {
-    chrome.tabs.query(queryOptions).then((res) => {
+const getTabUrl = (method) => {
+    return chrome.tabs.query(queryOptions).then((res) => {
         const [tab] = res;
 
         const data = {
+            method,
             url: tab.url,
-            time: new Date().getTime()
+            time: new Date().getTime(),
         };
-
-         post(data)
+        return data;
     });
-    
-})
+};
+
+chrome.tabs.onActivated.addListener(() => {
+    getTabUrl('onActivated').then((res) => {
+        if (res.url !== '') {
+            post(res);
+        }
+    });
+});
+
+chrome.tabs.onUpdated.addListener(() => {
+    getTabUrl('onUpdated').then((res) => {
+            post(res);
+    });
+});
+
+chrome.windows.onFocusChanged.addListener((window) => {
+    if (window == chrome.windows.WINDOW_ID_NONE) {
+        // inFocus = false;
+    } else {
+        // inFocus = true;
+        getTabUrl('onFocusChanged').then((res) => {
+            if (res.url !== '') {
+                post(res)
+            }
+        });
+    }
+});
